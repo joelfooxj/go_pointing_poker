7a566ba776ce02f7e61745dbd38506a40c38778a
Joel Foo <joelfooxj@gmail.com>
2023-03-10 19:06:12 -0500

    Removed scripts

--- resetAllKeys.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo sending the PUT to reset all keys...
-curl -X PUT localhost:8090/resetallkeys
--- setKey.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo sending the POST to set key...
-curl -X POST localhost:8090/setkey \
--H "Content-Type: application/json" \
--d '{"key":"key1", "value":123}'
