82d5b69f389e57aee56dc8795018634d86dc49cc
Joel Foo <joelfooxj@gmail.com>
2023-04-04 21:47:34 -0400

    New spin folder

---
+++ tb_poker/go.mod
@@ -0,0 +1,5 @@
+module github.com/tb_poker
+
+go 1.17
+
+require github.com/fermyon/spin/sdk/go v0.10.0
---
+++ tb_poker/tb_ppoker.go
@@ -0,0 +1,369 @@
+// Code modified from https://github.com/kljensen/golang-html5-sse-example
+package main
+
+import (
+	"crypto/rand"
+	"embed"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"math/big"
+	"net/http"
+	"sync"
+)
+
+const TBADMIN = "TBADMIN"
+const MAX_USERS = 20
+
+var (
+	//go:embed templates/*.tmpl.html
+	files     embed.FS
+	mainPage  *template.Template
+	loginPage *template.Template
+
+	mapManager = &MapManager{
+		mu:           sync.Mutex{},
+		pointsMap:    make(map[string]int),
+		hiddenMap:    make(map[string]string),
+		updateChan:   make(chan bool),
+		isMapVisible: true,
+	}
+
+	broker = &Broker{
+		make(map[chan bool]bool),
+		make(chan (chan bool)),
+		make(chan (chan bool)),
+	}
+
+	isTBAdminLoggedIn        = false
+	adminHash         string = ""
+)
+
+type MapManager struct {
+	mu           sync.Mutex
+	pointsMap    map[string]int
+	hiddenMap    map[string]string
+	updateChan   chan bool
+	isMapVisible bool
+}
+
+func (m *MapManager) setMapVisibility(isVisible bool) {
+	m.isMapVisible = isVisible
+	m.updateChan <- true
+}
+
+func (m *MapManager) toggleMapVisibility() {
+	m.isMapVisible = !(m.isMapVisible)
+	m.updateChan <- true
+}
+
+func (m *MapManager) getMapPayload() []byte {
+	var payload []byte
+	var err error
+	if m.isMapVisible {
+		payload, err = json.Marshal(mapManager.pointsMap)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		payload, err = json.Marshal(mapManager.hiddenMap)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return payload
+}
+
+func (m *MapManager) resetMap() {
+	m.mu.Lock()
+	for key := range m.pointsMap {
+		m.pointsMap[key] = 0
+		m.hiddenMap[key] = ""
+	}
+	m.isMapVisible = false
+	m.mu.Unlock()
+	m.updateChan <- true
+}
+
+func (m *MapManager) keyExists(key string) bool {
+	_, ok := m.pointsMap[key]
+	return ok
+}
+
+func (m *MapManager) deleteKey(key string) {
+	m.mu.Lock()
+	delete(m.pointsMap, key)
+	delete(m.hiddenMap, key)
+	m.mu.Unlock()
+	m.updateChan <- true
+}
+
+func (m *MapManager) addKey(key string) {
+	m.mu.Lock()
+	m.pointsMap[key] = 0
+	m.hiddenMap[key] = ""
+	m.mu.Unlock()
+	m.updateChan <- true
+}
+
+func (m *MapManager) setKey(key string, value int) {
+	m.mu.Lock()
+	m.pointsMap[key] = value
+	m.hiddenMap[key] = "?"
+	m.mu.Unlock()
+	m.updateChan <- true
+}
+
+type Broker struct {
+	// The idomatic way of implementing a set is a map
+	// This stores the set of active clients
+	clients map[chan bool]bool
+
+	// A channel to receive the channels of
+	// new clients to be stored in the active set/map
+	newClients chan chan bool
+
+	// A channel to receive the channels of disconnected clients
+	// to be removed from the active set/map
+	dcClients chan chan bool
+}
+
+func (b *Broker) Start() {
+	go func() {
+		for {
+			// Block until we receive from one of the
+			// three following channels.
+			select {
+			case c := <-b.newClients:
+				// A new client has connected.
+				// Store the new client
+				b.clients[c] = true
+			case c := <-b.dcClients:
+				// A client has disconnected.
+				// Remove the client from the set/map
+				delete(b.clients, c)
+				close(c)
+			case hasUpdate := <-mapManager.updateChan:
+				// Iterate through and relay the update signal to
+				// each client channel
+				log.Printf("Notifying %d clients", len(b.clients))
+				for c := range b.clients {
+					c <- hasUpdate
+				}
+			}
+		}
+	}()
+}
+
+func setKeyHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var data map[string]interface{}
+
+	rawBytes, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := json.Unmarshal(rawBytes, &data); err != nil {
+		panic(err)
+	}
+
+	key := data["key"].(string)
+	value := int(data["value"].(float64))
+
+	// Don't want unconnected users to
+	// add themselves
+	if !mapManager.keyExists(key) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	mapManager.setKey(key, value)
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func resetAllKeysHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "PUT" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Authenticate
+	verifyHash := req.Header.Get("X-Admin-Hash")
+	if verifyHash != adminHash {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	fmt.Println("Resetting all keys")
+	mapManager.resetMap()
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func toggleKeyVisibilityHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	verifyHash := req.Header.Get("X-Admin-Hash")
+	if verifyHash != adminHash {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	fmt.Println("Toggling key visibility")
+	mapManager.toggleMapVisibility()
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("true"))
+}
+
+type LoginDetails struct {
+	Key       string
+	AdminHash string
+}
+
+func serveMainPageHandler(w http.ResponseWriter, req *http.Request) {
+	key := req.URL.Query().Get("username")
+
+	forbiddenTBAadmin := key == TBADMIN && isTBAdminLoggedIn
+	emptyKey := key == ""
+	tooManyUsers := len(mapManager.pointsMap) >= MAX_USERS
+
+	if forbiddenTBAadmin || emptyKey || mapManager.keyExists(key) || tooManyUsers {
+		http.Redirect(w, req, "/", http.StatusForbidden)
+		return
+	}
+
+	var randNum *big.Int
+	var err error
+	var randString string
+	if key == TBADMIN {
+		randNum, err = rand.Int(rand.Reader, big.NewInt(100000000000))
+		if err != nil {
+			panic(err)
+		}
+		randString = randNum.String()
+		adminHash = randString
+	} else {
+		randString = ""
+	}
+
+	loginDetails := LoginDetails{key, randString}
+
+	fmt.Println("Serving main page for user:", loginDetails)
+	if err := mainPage.Execute(w, loginDetails); err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+}
+
+func loginPageHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Serving login page for", req.Host)
+
+	if err := loginPage.Execute(w, nil); err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+}
+
+// Opens the connection with client
+// and remains open until client closes connection
+func (b *Broker) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	// Set the headers related to event streaming.
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Transfer-Encoding", "chunked")
+
+	// Make sure that the writer supports flushing.
+	f, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+
+	clientChan := make(chan bool)
+	b.newClients <- clientChan
+
+	key := req.URL.Query().Get("username")
+	fmt.Println("Subscribed:", key)
+
+	if key == TBADMIN {
+		isTBAdminLoggedIn = true
+		mapManager.setMapVisibility(false)
+	} else {
+		mapManager.addKey(key)
+	}
+
+	payload := mapManager.getMapPayload()
+	fmt.Fprintf(w, "data: %s\n\n", payload)
+	f.Flush()
+
+	// Listen to the closing of the http connection via the CloseNotifier
+	notify := w.(http.CloseNotifier).CloseNotify()
+	go func() {
+		<-notify
+		fmt.Println(key, "has disconnected")
+
+		if isTBAdminLoggedIn && key == TBADMIN {
+			isTBAdminLoggedIn = false
+			mapManager.setMapVisibility(true)
+		} else {
+			mapManager.deleteKey(key)
+		}
+		b.dcClients <- clientChan
+	}()
+
+	// Don't close the connection, instead loop endlessly.
+	for {
+		// Notify the client via its routine
+		// that there is an update
+		_, open := <-clientChan
+
+		if !open {
+			fmt.Println("Channel for", key, "is closed")
+			break
+		}
+
+		payload := mapManager.getMapPayload()
+		fmt.Fprintf(w, "data: %s\n\n", payload)
+		f.Flush()
+	}
+}
+
+func main() {
+	var err error
+	mainPage, err = template.ParseFS(files, "templates/main_page.tmpl.html")
+	if err != nil {
+		panic(err)
+	}
+
+	loginPage, err = template.ParseFS(files, "templates/login_page.tmpl.html")
+	if err != nil {
+		panic(err)
+	}
+
+	// Broker starts listening and managing channels
+	broker.Start()
+
+	http.HandleFunc("/setkey", setKeyHandler)
+	http.HandleFunc("/togglekeyvisibility", toggleKeyVisibilityHandler)
+	http.HandleFunc("/resetkeys", resetAllKeysHandler)
+	http.HandleFunc("/main", serveMainPageHandler)
+	http.HandleFunc("/", loginPageHandler)
+	http.Handle("/sse_events", broker)
+
+	http.ListenAndServe("localhost:8090", nil)
+}
