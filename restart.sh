4f15b1286e17f22c00d510a378b5e98d18c3793d
Joel Foo <joelfooxj@gmail.com>
2023-11-18 17:19:12 -0500

    removed restart script

--- restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo Resetting server... 
-ssh -i "~/.ssh/personal.pem" ec2-user@ec2-3-145-54-79.us-east-2.compute.amazonaws.com 'sudo pkill server; sudo ~/go_pointing_poker/server &' 
-# ssh -i "~/.ssh/personal.pem" ec2-user@ec2-3-145-54-79.us-east-2.compute.amazonaws.com 'sudo ~/go_pointing_poker/server'
